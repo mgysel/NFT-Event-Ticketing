489aab427cbee400bf5f95ac32a51312dad8b7c4
Mike Gysel <z5251938@ad.unsw.edu.au>
2021-07-09 18:22:02 +1000

    Added buyTicket, stateMachine, some modifiers

--- src/contracts/Event.sol
+++ src/contracts/Event.sol
@@ -4,27 +4,79 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Color is ERC721 {
 
-    enum States { Prep, Active, Open, Paused, Cancelled, Closed }
+    enum Stages { Prep, Active, Paused, CheckinOpen, Cancelled, Closed }
+    Stages public stage = Stages.Prep;
     // Ticket struct 
-    // struct Ticket {
-
-    // }
-    int numTickets;
-    int price;
+    struct Ticket {
+        address owner;
+        uint price;
+        bool forSale;
+        bool used;
+    }
+    Ticket[] tickets; 
+    uint numTicketsLeft;
+    uint price;
     // For each user, store corresponding ticket struct
     // mapping(address => Ticket) tickets;
     bool canBeResold;
     address public owner;
 
+    // EVENTS
+    event CreateNFTTicket(address buyer, uint NFTID);
+
     // Creates a new Event Contract
-    constructor(uint8 numTickets, uint8 price, bool canBeResold) public {
+    constructor(uint numTickets, uint price, bool canBeResold) public {
         owner = msg.sender;
-        numTickets = numTickets;
+        numTicketsLeft = numTickets;
         price = price;
         canBeResold = canBeResold;
     }
 
-    // Functions here
+    // Customer purchases a ticket from Organizer
+    // Checks: State is Active, has enough money, 
+    function buyTicket() public payable requiredStage(Stages.Active) ticketsLeft hasEnoughMoney(msg.value) {
+        // Create Ticket t
+        Ticket memory t;
+        t.owner = msg.sender;
+        t.price = price;
+        t.forSale = false;
+        t.used = false;
 
-    // Modifiers here
+        // Store t in tickets array, reduce numTicketsLeft
+        tickets.push(t);
+        uint NFTID = tickets.length;
+        numTicketsLeft--;
+
+        // Mint NFT
+        _mint(msg.sender, NFTID);
+        emit CreateNFTTicket(msg.sender, NFTID);
+    }
+
+    // Set new stage
+    function setStage(Stages _stage) public {
+        stage = _stage;
+    }
+
+
+
+
+    // MODIFIERS
+
+    // Requires stage to be _stage
+    modifier requiredStage(Stages _stage) {
+        require(stage == _stage);
+        _;
+    }
+
+    // Requires there to be more than 0 tickets left
+    modifier ticketsLeft() {
+        require(numTicketsLeft > 0);
+        _;
+    }
+
+    modifier hasEnoughMoney(uint money) {
+        require(money >= price);
+        _;
+    }
+
 }
