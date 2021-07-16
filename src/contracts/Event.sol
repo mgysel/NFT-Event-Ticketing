664d6d55a828870e064e0e562f84994c0f17a478
prams25 <pramithprashanth@gmail.com>
2021-07-16 17:05:53 +1000

    fix

--- src/contracts/Event.sol
+++ src/contracts/Event.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+//import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract Event is ERC721 {
 
