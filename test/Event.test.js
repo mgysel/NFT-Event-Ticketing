fd49ee0264cc96d6280fd38f8dbadb2d4cfd94cc
Mike Gysel <z5251938@ad.unsw.edu.au>
2021-07-27 17:57:29 +1000

    Updated tests to pass

--- test/Event.test.js
+++ test/Event.test.js
@@ -106,23 +106,20 @@ contract('Event', (accounts) => {
             expect(royalty).to.eql(expected)
         })
 
+        it('invalid constructor arguments', async() => {
+            // Owner must be address
+            const invalidNumTickets = 0
+            await Event.new(owner, invalidNumTickets, _price, _canBeResold, _royaltyPercent, _eventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
 
-    //     // // TODO: constructor arguments are valid (this should make sure no overflow)
-    //     // it('invalid constructor arguments', async() => {
-    //     //     // Owner must be address
-    //     //     const invalidNumTickets = 0
-    //     //     await Event.new(owner, invalidNumTickets, _price, _canBeResold, _royaltyPercent, _eventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
+            const invalidRoyaltyPercent = 101 
+            await Event.new(owner, _numTickets, _price, _canBeResold, invalidRoyaltyPercent, _eventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
 
-    //     //     const invalidRoyaltyPercent = 101 
-    //     //     await Event.new(owner, _numTickets, _price, _canBeResold, invalidRoyaltyPercent, _eventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
+            const invalidEventName = ''
+            await Event.new(owner, _numTickets, _price, _canBeResold, _royaltyPercent, invalidEventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
 
-    //     //     const invalidEventName = ''
-    //     //     await Event.new(owner, _numTickets, _price, _canBeResold, _royaltyPercent, invalidEventName, _eventSymbol).should.be.rejectedWith(EVM_REVERT)
-
-    //     //     const invalidEventSymbol = ''
-    //     //     await Event.new(owner, _numTickets, _price, _canBeResold, _royaltyPercent, _eventName, invalidEventSymbol).should.be.rejectedWith(EVM_REVERT)
-    //     // })
-
+            const invalidEventSymbol = ''
+            await Event.new(owner, _numTickets, _price, _canBeResold, _royaltyPercent, _eventName, invalidEventSymbol).should.be.rejectedWith(EVM_REVERT)
+        })
 
     })
 
@@ -136,10 +133,9 @@ contract('Event', (accounts) => {
             // Paused (2) Stage
             await event.setStage(2)
             await event.buyTicket({ value: (_price), from: buyer1 }).should.be.rejectedWith(EVM_REVERT)
-            // // Cancelled (4) Stage
-            // await event.setStage(4)
-            // await event.buyTicket({ value: (_price), from: buyer1 }).should.be.rejectedWith(EVM_REVERT)
-        
+            // Cancelled (4) Stage
+            await event.setStage(4)
+            await event.buyTicket({ value: (_price), from: buyer1 }).should.be.rejectedWith(EVM_REVERT)
             // Closed (5) Stage
             await event.setStage(5)
             await event.buyTicket({ value: (_price), from: buyer1 }).should.be.rejectedWith(EVM_REVERT)
@@ -284,7 +280,7 @@ contract('Event', (accounts) => {
             let sQRCodeKey = "12345"
             await event.setTicketToUsed(ticketID, sQRCodeKey, { from: buyer1 })
             let x = await event.getTicketStatus(ticketID, { from: buyer1 })
-            assert.equal(x.valueOf(), 2, 'ticket should be used')
+            assert.equal(x.valueOf(), 1, 'ticket should be used')
         })
     })
 
@@ -325,11 +321,11 @@ contract('Event', (accounts) => {
             })
         })
 
-        it('confirm ticket set to used', async() => {
+        it('confirm ticket available for sale', async() => {
             let ticketID = 0
             await event.setTicketForSale(ticketID, { from: buyer1 })
             let x = await event.getTicketStatus(ticketID, { from: buyer1 })
-            assert.equal(x.valueOf(), 3, 'ticket should be vaialable for sale')
+            assert.equal(x.valueOf(), 2, 'ticket should be available for sale')
         })
 
         it('checking cannot set for sale if used', async () => {
@@ -368,7 +364,7 @@ contract('Event', (accounts) => {
             let ticketID = 0
             let e = await event.setTicketForSale(ticketID, { from: buyer1 })
             let x = await event.getTicketStatus(ticketID, { from: buyer1 })
-            assert.equal(x.valueOf(), 3, 'ticket should be vaialable for sale')
+            assert.equal(x.valueOf(), 2, 'ticket should be available for sale')
         })
 
         it('buy ticket from buyer 1', async () => {
