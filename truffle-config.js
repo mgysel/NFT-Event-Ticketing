ab79f9500c3080593134259dea9ef06e6483cb4a
Mike Gysel <mikegysel@gmail.com>
2023-10-01 13:33:23 +0200

    Updated truffle config to allow for migrations to testnet

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,19 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          //private keys array
+          [privateKey],
+          //url to ethereum node
+          endpointUrl
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
